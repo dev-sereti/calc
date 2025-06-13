5aa73b2d53915bb1a0d0afe704272febdb9d2081
dev <devsereti@gmail.com>
2025-06-13 17:58:47 +0300

    File updated

--- script.js
+++ script.js
@@ -7,32 +7,3 @@ function appendNumber(num) {
     display.innerText += num;
   }
 }
-
-function appendOperator(op) {
-  const lastChar = display.innerText.slice(-1);
-  if ('+-*/%'.includes(lastChar)) {
-    display.innerText = display.innerText.slice(0, -1) + op;
-  } else {
-    display.innerText += op;
-  }
-}
-
-function clearDisplay() {
-  display.innerText = '0';
-}
-
-function deleteLast() {
-  if (display.innerText.length > 1) {
-    display.innerText = display.innerText.slice(0, -1);
-  } else {
-    display.innerText = '0';
-  }
-}
-
-function calculateResult() {
-  try {
-    display.innerText = eval(display.innerText.replace(/÷/g, '/').replace(/×/g, '*'));
-  } catch {
-    display.innerText = 'Error';
-  }
-}
