8ae31e27fa2c5483efce03b65d3a8d9e91f4e2c7
dev <devsereti@gmail.com>
2025-06-13 17:57:28 +0300

    file updated

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
-const display=document.getElementById('dispaly');
-function appendNumber(num){
-    if(display.innerText==='0'){
-        display.innerText=num;
-    
-    }else{
-        display.innerText+=num;
-    
-    }
+const display = document.getElementById('display');
+
+function appendNumber(num) {
+  if (display.innerText === '0') {
+    display.innerText = num;
+  } else {
+    display.innerText += num;
+  }
+}
+
+function appendOperator(op) {
+  const lastChar = display.innerText.slice(-1);
+  if ('+-*/%'.includes(lastChar)) {
+    display.innerText = display.innerText.slice(0, -1) + op;
+  } else {
+    display.innerText += op;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = '0';
+}
+
+function deleteLast() {
+  if (display.innerText.length > 1) {
+    display.innerText = display.innerText.slice(0, -1);
+  } else {
+    display.innerText = '0';
+  }
+}
+
+function calculateResult() {
+  try {
+    display.innerText = eval(display.innerText.replace(/÷/g, '/').replace(/×/g, '*'));
+  } catch {
+    display.innerText = 'Error';
+  }
 }
